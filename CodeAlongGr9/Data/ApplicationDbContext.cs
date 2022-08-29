d19135af39f4f945f2b15b60d9ee355868e609ed
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2022-08-29 10:38:47 +0200

    Added EF

---
+++ CodeAlongGr9/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CodeAlongGr9.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeAlongGr9.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- CodeAlongGr9/Program.cs
+++ CodeAlongGr9/Program.cs
@@ -1,6 +1,14 @@
+using CodeAlongGr9.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
